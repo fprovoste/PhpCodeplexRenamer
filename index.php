2eb2a122527c42299308b17f488a9f566e4a0acf
Fredy Provoste <fprovoste@gmail.com>
2023-07-21 18:14:42 -0300

    Temporalmente se puede agregar carpeta releases de codeplex en este dir

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <?php
 
 require "./vendor/autoload.php";
-
-$people_json = file_get_contents('releaseList.json');
+$rootDir = "./releases/";
+$releaseListFile = $rootDir."releaseList.json";
+$people_json = file_get_contents($releaseListFile);
 
 $json = json_decode($people_json);
 
 $elementos = collect($json);
 
-$elementos->each(function ($elemento){
+$elementos->each(function ($elemento) use($rootDir){
     $carpeta = $elemento->Id;
+    $nuevoNombreCarpeta = $elemento->Name;
     $descripcion = $elemento->Description;
-    $txtDescripcion = ".".DIRECTORY_SEPARATOR.$carpeta.DIRECTORY_SEPARATOR."Descripcion.txt";
+    $txtDescripcion = "$rootDir.".DIRECTORY_SEPARATOR.$carpeta.DIRECTORY_SEPARATOR."Descripcion.txt";
     if (!file_exists($txtDescripcion))
     {
         file_put_contents($txtDescripcion,$descripcion);
     }
     $archivos = collect($elemento->Files);
-    $archivos->each(function ($archivo) use($carpeta)
+    $archivos->each(function ($archivo) use($rootDir,$carpeta)
     {
-        if (file_exists($archivo->Url))
+        if (file_exists($rootDir."/".$archivo->Url))
         {
-            $nuevoArchivo = "./".$carpeta."/".$archivo->FileName;
-
-            //echo $archivo->Url . " -> ".$nuevoArchivo. "\n";
-            rename($archivo->Url,$nuevoArchivo);
+            $nuevoArchivo = "$rootDir./".$carpeta."/".$archivo->FileName;
+            echo $archivo->Url . " -> ".$nuevoArchivo. "\n";
+            rename($rootDir."/".$archivo->Url,$nuevoArchivo);
         }
     });
+    rename("$rootDir./".$carpeta,"$rootDir./".$nuevoNombreCarpeta);
 });
 
